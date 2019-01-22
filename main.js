3dc2346ec0972926f7e4823749e722a15820c544
nkvthedevil <so.cold.soul@gmail.com>
2019-01-22 14:11:52 +0300

    Add new projects & restyle

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
-$(document).ready(function () {
-    function getAge(birth, exp) {
-        var cur = new Date(),
-            birth = new Date(birth),
-            base = Math.pow(10, exp);
-        age = Math.floor(((cur.getTime() - birth.getTime()) / (365.25 * 24 * 60 * 60 * 1000)) * base) / base;
+function getAge(birth, exp) {
+    let cur = new Date(),
+        birthday = new Date(birth),
+        base = Math.pow(10, exp);
+    return Math.floor(((cur.getTime() - birthday.getTime()) / (365.25 * 24 * 60 * 60 * 1000)) * base) / base;
+}
 
-        return age;
-    }
-    $('#age').text(getAge(new Date('1993/7/2'), 0));
-    $('#exp').text(getAge(new Date('2017/12/1'), 1) + 'Y');
-});
+document.querySelector('#age').innerText = getAge(new Date('1993/7/2'), 0);
+document.querySelector('#exp').innerText = getAge(new Date('2017/12/1'), 1) + 'Y';
+
